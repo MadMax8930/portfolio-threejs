5e251dfadc99bece78a59d4df944a5fa688a6e81
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-13 23:28:51 +0100

    contact form animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,20 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
 
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div 
+        variants={slideIn('left','tween', 0,2, 1)} 
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
