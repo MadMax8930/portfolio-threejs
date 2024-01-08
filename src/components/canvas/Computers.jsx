33b7f468edf2d96a199d10c5769bd5050ecf2054
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-01-08 19:15:06 +0100

    v2.5

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,7 +33,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
   
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 300px)');
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
     setIsMobile(mediaQuery.matches);
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
@@ -60,7 +60,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={!isMobile} />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all/>
