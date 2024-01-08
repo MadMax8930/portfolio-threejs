4cf334895af3db7021ea7cdb2a9e70dfe9899f3d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-01-08 20:04:29 +0100

    test

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -33,7 +33,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
   
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 200px)');
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
     setIsMobile(mediaQuery.matches);
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
