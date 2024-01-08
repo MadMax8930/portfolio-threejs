7e5c84a361cc817f4bce36be2ba133cb881b8c8e
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-01-08 18:56:37 +0100

    canvas v2.21

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,7 +21,7 @@ const Computers = ({isMobile}) => {
       />
       <primitive 
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -33,7 +33,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
   
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 300px)');
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
     setIsMobile(mediaQuery.matches);
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
